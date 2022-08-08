5f3ab44e55eb5bb3ba98cf2e06e15c13c4627969
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2022-08-08 11:40:51 +0300

    add example for Chain of Responsibility

--- DesignPatterns.playground/Pages/Chain of Responsibility (behavioral).xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Chain of Responsibility (behavioral).xcplaygroundpage/Contents.swift
@@ -9,5 +9,91 @@ import XCTest
 
  Избавляет от жёсткой привязки отправителя запроса к его получателю, позволяя выстраивать цепь из различных обработчиков динамически.
  
- GestureRecognize, который определяет на какой элемент нажал пользователь работает через hitTest, где опрашивая родителя вызывают последнего UIReponser
+ GestureRecognize, который определяет на какой элемент нажал пользователь работает через hitTest, где опрашивая родителя вызывают последнего UIReponser.
+ 
+ Пример основан на работе UITest'ов с mock запросами, модуль тестов может прокидывать статичные json или динамические, а так-же есть дефолтные. При обработке reqeust, мы вначале должны искать в динамических/статичных, а только потом обращаться к дефолтным и искать ответ.
  */
+
+final class UITestResponder {
+    
+    typealias JSON = String
+    
+    var responses = [String: JSON]()
+    var next: UITestResponder?
+    
+    func handle(request: String) -> JSON? {
+        if let response = responses.first(where: { $0.key == request })?.value {
+            return response
+        } else if let next = next {
+            return next.handle(request: request)
+        } else {
+            return nil
+        }
+    }
+}
+
+final class UITestContainer {
+    
+    var defaultTests: UITestResponder?
+    var staticTests: UITestResponder?
+    var dynamicTests: UITestResponder?
+    
+    var firstResponder: UITestResponder?
+    
+    func handle(request: String) -> UITestResponder.JSON? {
+        return firstResponder?.handle(request: request)
+    }
+}
+
+final class Tests: XCTestCase {
+    
+    lazy var containerTest: UITestContainer = {
+        let container = UITestContainer()
+        
+        container.firstResponder = dynamicTests
+        
+        container.defaultTests = defaultTests
+        container.staticTests = staticTests
+        container.dynamicTests = dynamicTests
+        
+        return container
+    }()
+    
+    lazy var defaultTests: UITestResponder = {
+        let responder = UITestResponder()
+        
+        responder.responses = ["https://google/apply": "200",
+                           "https://google/translate": "500" ]
+        
+        return responder
+    }()
+    
+    lazy var staticTests: UITestResponder = {
+        let responder = UITestResponder()
+        
+        responder.responses = ["https://google/apply": "300",
+                           "https://google/wallet": "200" ]
+        responder.next = defaultTests
+        
+        return responder
+    }()
+    
+    lazy var dynamicTests: UITestResponder = {
+        let responder = UITestResponder()
+        
+        responder.responses = ["https://google/wallet": "300",
+                           "https://google/discount": "200" ]
+        responder.next = staticTests
+        
+        return responder
+    }()
+    
+    func test0() {
+        XCTAssertEqual(containerTest.handle(request: "https://google/wallet"), "300")
+        XCTAssertEqual(containerTest.handle(request: "https://google/discount"), "200")
+        XCTAssertEqual(containerTest.handle(request: "https://google/apply"), "300")
+        XCTAssertEqual(containerTest.handle(request: "https://google/translate"), "500")
+    }
+}
+
+Tests.defaultTestSuite.run()
